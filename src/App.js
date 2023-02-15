c965a6dce38c0675fda41114ccfffc88ee85923b
Jeffrey-Z1030 <113541414+Jeffrey-Z1030@users.noreply.github.com>
2023-02-14 22:46:50 -0500

    delete almost 100% + some styling

--- src/App.js
+++ src/App.js
@@ -9,9 +9,17 @@ import LogOut from './Components/LogOut';
 import GetRoutines from './Components/getAllRoutines';
 import { Route, BrowserRouter, Link, Routes } from 'react-router-dom';
 import PostActivitie from './Components/postActivities';
+import PostRoutines from './Components/postRoutines';
+
+
+const replyToken ='test_token'
+
 
 function App() {
   const [token,setToken] = useState(localStorage.getItem('replyToken'))
+
+
+  
 
   return (
     
@@ -20,6 +28,14 @@ function App() {
           <ul className="links">
           <li><Link to='/signup'>SignUp</Link></li>
           <li><Link to='/login'>Login</Link></li>
+          <li><Link to='/getactivities'>Activities</Link></li>
+          <li><Link to='/getroutines'>Routines</Link></li>
+          <li><Link to='/postactivitie'>Create Activity</Link></li>
+          <li><Link to='/postroutines'>Create Routine</Link></li>
+          <li><Link to='/userme'>TestGetMe</Link></li>
+          <li><Link to='/getusersroutines'>Test Get User Routines</Link></li>
+
+
     
           </ul>
           {(token) ?<LogOut/> :null}
@@ -29,15 +45,18 @@ function App() {
         <div className='test'>
           <Routes>
           <Route path='/signup' element={<SignUp/>}/>
-          <Route path='/login' element={<Login/>}/>
+          <Route path='/login' element={<Login setToken={setToken}/>}/>
+          <Route path='/getactivities' element={<GetActivities/>}/>
+          <Route path='/getroutines' element={<GetRoutines/>}/>
+          <Route path='/postactivitie' element={<PostActivitie/>}/>
+          <Route path='/postroutines' element={<PostRoutines/>}/>
+          <Route path='/userme' element={<UserMe/>}/>
+          <Route path='/getusersroutines' element={<GetUsersRoutines/>}/>
 
           </Routes>
-          
-          <PostActivitie />
-          <UserMe/>
-          <GetUsersRoutines/>
-          <GetActivities/>
-          <GetRoutines/>
+ 
+     
+
           </div> 
       </div>
     
